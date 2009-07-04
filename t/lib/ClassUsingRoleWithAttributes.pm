ff4b5a23e3267e4d7e05095c9e18be77225eae92
Tomas Doran (t0m <bobtfish@bobtfish.net>
2009-07-04 14:46:07 +0100

    FIXME has gone away, add another role so I can test combination

--- t/lib/ClassUsingRoleWithAttributes.pm
+++ t/lib/ClassUsingRoleWithAttributes.pm
@@ -1,7 +1,8 @@
+package Other::Role;
+use Moose::Role;
+
 package ClassUsingRoleWithAttributes;
 use Moose;
-# FIXME - This should not have to be here.
-use MooseX::MethodAttributes;
 
 use namespace::clean -except => 'meta';
 
